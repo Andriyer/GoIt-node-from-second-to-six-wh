f301f9e2f5c361af7fa14b4cc51f224a55078b8d
Andriyer <83455102+Andriyer@users.noreply.github.com>
2022-04-29 00:11:21 +0300

    hw3

--- repository/contacts.js
+++ repository/contacts.js
@@ -1,48 +1,34 @@
-const { ObjectId } = require('mongodb');
-const DB = require ('./db')
-
-const getCollection = async (db, nameCollection) =>{
-  const client = await db 
-  const collection = client.db().collection(nameCollection);
-  return collection
-}
-
+const Contact = require('../models/contact')
 
 const listContacts = async () => {
-  const collection = await getCollection(DB, 'contacts')
-  const result = await collection.find({}).toArray()
+  const result = await Contact.find()
   return result
 }
 
 const getContactById = async (contactId) => {
-  const collection = await getCollection(DB, 'contacts')
-  const objId = ObjectId(contactId)
-  const [result] = await collection.find({_id: objId}).toArray()
+  const result = await Contact.findOne({_id: contactId})
   return result
 }
 
 const removeContact = async (contactId) => {
-  const collection = await getCollection(DB, 'contacts')
-  const objId = new ObjectId(contactId)
-  const {value: result} = await collection.findOneAndDelete({_id: objId})
+  const result = await Contact.findOneAndRemove({_id: contactId})
   return result
 }
 
 const addContact = async (body) => {
-  const collection = await getCollection(DB, 'contacts')
-  const newContact = {
-    ...body
-  }
- const result = await collection.insertOne(newContact)
- return await getContactById(result.insertedId)  
+  const result = await Contact.create(body)
+  return result
 }
 
 const updateContact = async (contactId, body) => {
-  const collection = await getCollection(DB, 'contacts')
-  const objId = new ObjectId(contactId)
-  const {value: result} = await collection.findOneAndUpdate({_id: objId}, {$set: body}, {returnDocument: 'after'})
+  const result = await Contact.findOneAndUpdate({_id: contactId}, {...body}, {new: true})
   return result
 }
+
+const updateContactFavorite = async (contactId, body) => {
+    const result = await Contact.findOneAndUpdate({_id: contactId}, {...body})
+    return result
+  }
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,41 +1,21 @@
 const express = require('express')
-const contactsModel = require('../../models/contacts')
+const {listContacts, getContactById, addContact, removeContact, updateContact, updateContactFavorite} = require('../../controllers/contacts')
 const {schemaCreateContact, schemaMongoId} = require('./validation-schem')
 const {validateBody, validateParams} = require('../../middlewares/validation')
+
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  const contacts = await contactsModel.listContacts()
-  res.json({ status: 'success', code: 200, payload: {contacts} })
-})
+router.get('/', listContacts)
 
-router.get('/:contactId', validateParams(schemaMongoId), async (req, res, next) => {
-  const contact = await contactsModel.getContactById(req.params.contactId)
-  if (contact){
-    res.json({ status: 'success', code: 200, payload: {contact} })
-  }
-  return res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
-})
+router.get('/:contactId', validateParams(schemaMongoId), getContactById)
 
-router.post('/', validateBody(schemaCreateContact), async (req, res, next) => {
-  const contact = await contactsModel.addContact(req.body)
-  res.status(201).json({ status: 'success', code: 201, payload: {contact} })
-})
+router.post('/', validateBody(schemaCreateContact), addContact)
 
-router.delete('/:contactId', validateParams(schemaMongoId), async (req, res, next) => {
-  const contact = await contactsModel.removeContact(req.params.contactId)
-  if (contact){
-    res.json({ status: 'success', code: 200, payload: {contact} })
-  }
-  return res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
-})
+router.delete('/:contactId', validateParams(schemaMongoId), removeContact)
 
-router.put('/:contactId', [validateParams(schemaMongoId), validateBody(schemaCreateContact)], async (req, res, next) => {
-  const contact = await contactsModel.updateContact(req.params.contactId, req.body)
-  if (contact){
-    res.json({ status: 'success', code: 200, payload: {contact} })
-  }
-  return res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
-})
+router.put('/:contactId', [validateParams(schemaMongoId), validateBody(schemaCreateContact)], updateContact)
+
+router.patch('/:contactId/favorite', [validateParams(schemaMongoId), validateBody(schemaCreateContact)], updateContact)
+
 
 module.exports = router
